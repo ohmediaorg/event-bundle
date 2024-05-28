87f38108fbe2288a3ebd9a346d9b506c3091cbee
Justin Hoffman <justin@ohmedia.ca>
2024-05-28 17:06:25 -0600

    remove unused function

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -3,8 +3,6 @@
 namespace OHMedia\EventBundle\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\ORM\Query\Parameter;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 use OHMedia\EventBundle\Entity\Event;
@@ -38,27 +36,6 @@ class EventRepository extends ServiceEntityRepository
         if ($flush) {
             $this->getEntityManager()->flush();
         }
-    }
-
-    public function countBySlug(string $slug, int $id = null)
-    {
-        $params = [
-            new Parameter('slug', $slug),
-        ];
-
-        $qb = $this->createQueryBuilder('e')
-            ->select('COUNT(e.id)')
-            ->where('e.slug = :slug');
-
-        if ($id) {
-            $qb->andWhere('e.id <> :id');
-
-            $params[] = new Parameter('id', $id);
-        }
-
-        return $qb->setParameters(new ArrayCollection($params))
-            ->getQuery()
-            ->getSingleScalarResult();
     }
 
     public function getUpcomingQueryBuilder(): QueryBuilder
