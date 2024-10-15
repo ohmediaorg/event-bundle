914f84fe6444f8b2b93d9cc35efb17583bc026ee
justin-oh <justin@ohmedia.ca>
2024-10-15 17:10:56 -0600

    add an events page template check

--- src/Twig/WysiwygExtension.php
+++ src/Twig/WysiwygExtension.php
@@ -47,7 +47,13 @@ class WysiwygExtension extends AbstractWysiwygExtension
     {
         $pageRevision = $this->pageRenderer->getCurrentPageRevision();
 
-        if (!$pageRevision->containsShortcode('events()')) {
+        $callable = $pageRevision->getTemplate().'::getTemplate';
+
+        $isTemplate = is_callable($callable)
+            ? '@OHMediaEvent/events.html.twig' === call_user_func($callable)
+            : false;
+
+        if (!$isTemplate && !$pageRevision->containsShortcode('events()')) {
             return;
         }
 
