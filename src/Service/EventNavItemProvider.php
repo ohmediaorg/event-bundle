a5bc302dba90676f5a3202f06e5d58477573c584
Justin Hoffman <justin@ohmedia.ca>
2025-10-03 09:56:52 -0600

    events sidebar nav doesn't need to be a dropdown of tags are disabled

--- src/Service/EventNavItemProvider.php
+++ src/Service/EventNavItemProvider.php
@@ -10,11 +10,32 @@ use OHMedia\EventBundle\Entity\Event;
 use OHMedia\EventBundle\Entity\EventTag;
 use OHMedia\EventBundle\Security\Voter\EventTagVoter;
 use OHMedia\EventBundle\Security\Voter\EventVoter;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class EventNavItemProvider extends AbstractNavItemProvider
 {
+    public function __construct(
+        private AuthorizationCheckerInterface $authorizationChecker,
+        #[Autowire('%oh_media_event.event_tags%')]
+        private bool $eventTagsEnabled,
+    ) {
+        parent::__construct($authorizationChecker);
+    }
+
     public function getNavItem(): ?NavItemInterface
     {
+        if (!$this->eventTagsEnabled) {
+            if ($this->isGranted(EventVoter::INDEX, new Event())) {
+                $events = new NavLink('Events', 'event_index');
+                $events->setIcon('calendar3');
+
+                return $events;
+            } else {
+                return null;
+            }
+        }
+
         $nav = (new NavDropdown('Events'))
             ->setIcon('calendar3');
 
